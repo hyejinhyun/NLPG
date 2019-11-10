4f5513e6362ae1a4c6b3874346d745baef783d82
hyejinhyun <43198806+hyejinhyun@users.noreply.github.com>
2019-11-10 13:56:08 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,17 +1,17 @@
 from flask import Blueprint, request, render_template, flash, redirect, url_for
 from flask import current_app as current_app
 
-from nlpg import dbModule
+from nlpg.module import dbModule
 
-test= Blueprint('main', __name__, url_prefix='/home/nlpg/web')   #url_prefix는 파일 경로
+main= Blueprint('main', __name__, url_prefix='/')   #url_prefix는 파일 경로
 
-@test.route('/nlpg', methods=['GET'])
+@main.route('/main', methods=['GET'])
 def index():
     return render_template('/nlpg/index.html',
                             resultData=None)
 
 # SELECT 함수 예제
-@test.route('/nlpg', methods=['GET'])
+@main.route('/select', methods=['GET'])
 def select():
     db_class= dbModule.Database()
 
@@ -25,4 +25,3 @@ def select():
 
     return render_template('/nlpg/index.html',
                             resultData=row[:11])
-
