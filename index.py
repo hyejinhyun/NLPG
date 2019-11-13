a83ea6fa270b11e5c56bc2287ad356e897a51b83
hyejinhyun <43198806+hyejinhyun@users.noreply.github.com>
2019-11-13 14:29:29 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,24 +1,28 @@
 # Import flask dependencies
-from flask import Blueprint, request, render_template, flash, redirect, url_for
+from flask import Blueprint, render_template    # ,request, render_template, flash, redirect, url_for
 from flask import current_app as current_app
 
 from module import dbModule
+from module import crawling_textrank_mysql
 
 # Define the blueprint
-main= Blueprint('index', __name__, url_prefix='/')   #url_prefix는 파일 경로
+main= Blueprint('index.html', __name__, url_prefix='/')   #url_prefix는 파일 경로
 
-# SELECT 함수 예제
-@main.route('/', methods=['GET'])
+crawling_textrank_mysql.myThread("hi", 300000)
+
+# SELECT 함수
+@main.route('/')
+@main.route('/index.html', methods=['GET'])
 def select():
     db_class= dbModule.Database()
 
     sql     = "SELECT title, content,url \
-                FROM nlpgDB.newsTable \
-                ORDER By id DESC \
-                LIMIT 10"
+               FROM nlpg$nlpg.news \
+               ORDER By id DESC \
+               LIMIT 10;"
     row     = db_class.executeAll(sql)
 
     print(row)
 
-    return render_template('/nlpg/index.html',
+    return render_template('/index.html',
                             resultData=row[:11])
