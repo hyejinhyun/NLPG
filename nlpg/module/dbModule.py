4134260e82bc34c2250bace40f8503416dd9d583
hyejinhyun <43198806+hyejinhyun@users.noreply.github.com>
2019-11-18 18:02:51 +0900

    final ver

---
+++ nlpg/module/dbModule.py
@@ -0,0 +1,27 @@
+import pymysql
+
+class Database():
+    def __init__(self):
+        self.db= pymysql.connect(host='nlpg.mysql.pythonanywhere-services.com',
+                                  user='nlpg',
+                                  password='ewha1886!',
+                                  db='nlpg$nlpg',
+                                  charset='utf8')
+        self.cursor= self.db.cursor(pymysql.cursors.DictCursor)
+
+    def execute(self, query, args={}):
+        self.cursor.execute(query, args)
+
+    def executeOne(self, query, args={}):
+        self.cursor.execute(query, args)
+        row= self.cursor.fetchone()
+        return row
+
+    def executeAll(self, query, args={}):
+        self.cursor.execute(query, args)
+        row= self.cursor.fetchall()
+        return row
+
+    def commit():
+        self.db.commit()
+
